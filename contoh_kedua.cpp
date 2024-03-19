0dfb2144969382ccabb413f114193b307cbf080f
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-03-19 14:58:09 +0700

    membuat looping bilangan

---
+++ contoh_kedua.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    while (bilangan%2) {
+        cout << "Bilangan " << bilangan << " adalah Ganjil ";
+        bilangan = rand() % 10;
+    }
+        cout << "Bilangan " << bilangan << " adalah genap ";
+
+}
