a25f9f1d0398d1979777fb6312291469683c5c5f
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-03-19 15:09:23 +0700

    Membuat looping do while

---
+++ contoh_ketiga.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string pilihan;
+    do {
+        cout << "makan" << endl;
+        cout << "main game" << endl;
+        cout << "ibadah" << endl;
+
+        cout << "Apakah anda masih hidup? (Yes/No)";
+        cin >> pilihan;
+    } while (pilihan == "Yes");
+}
